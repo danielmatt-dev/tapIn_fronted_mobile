60ebf05c7d3a97e16719adba9939c54dc07bf8bc
Daniel <danielmatt1701@gmail.com>
2025-04-21 11:25:33 -0600

    Añadir módulo de inyección y version JAVA
    - Módulo de inyección de dependencias con Dio y Shared Preferences
    - Versión de Java 17

---
+++ lib/src/shared/utils/injections.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sl = GetIt.instance;
+
+initInjections() async {
+
+  /*  Dio  */
+  sl.registerSingleton<Dio>(
+      Dio(
+        BaseOptions(
+            connectTimeout: const Duration(seconds: 45),
+            receiveTimeout: const Duration(seconds: 45),
+            sendTimeout: const Duration(seconds: 45)
+        ),
+      ));
+
+  /*  Shared Preferences  */
+  sl.registerSingleton<SharedPreferences>(
+      await SharedPreferences.getInstance());
+
+}
